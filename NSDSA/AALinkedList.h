e6269635983a788bca2dcc71c83678ca2fe095b1
ahmad.ayyash <ahmad.ayyash@gmail.com>
2016-04-21 22:53:37 +0200

    Adding travis yml

--- NSDSA/AALinkedList.h
+++ NSDSA/AALinkedList.h
@@ -4,6 +4,7 @@
 @interface AALinkedList<T> : NSObject <iterator>
 - (void)removeAtIndex:(NSUInteger)index;
 - (void)addElement:(T)element atIndex:(NSUInteger)index;
+- (id)getElementAtIndex:(NSUInteger)index;
 - (void)insert:(T)element;
 - (void)contains:(T)element;
 - (void)clear;
